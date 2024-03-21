7e7d15ba242d8b7a6238f669dc3dcd8956cac1f5
Inokentii88 <ittehnologii2024>
2024-03-21 17:49:50 +0200

    added working cycle

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args)  {
-        
+        int sum = 0;
+        for (int num1 = 0; num1 < 101; num1++) {
+            sum += num1;
+            System.out.println(sum); }
+        for (int num2 = 0; num2 < 100; num2++){
+            if (num2 % 3 == 0){
+                System.out.println(num2);
+            }
+        }
     }
 }
